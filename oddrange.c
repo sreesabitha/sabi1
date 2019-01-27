8f21c2ea428aa51266906833a100ecea00005710
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-01-27 04:49:04 -0800

    Create oddrange.c

---
+++ oddrange.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b,i;
+    printf("enter n");
+    scanf("%d%d",&a,&b);
+    for(i=a;i<b;i++)
+    {
+        if(i%2!=0)
+        {
+        printf("%d ",i);
+    }
+    }
+return 0;
+}
+
