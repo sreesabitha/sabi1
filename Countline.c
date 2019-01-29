a1f55f781a4a8c705791944586c70843b0991f4f
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-01-29 08:42:01 +0530

    Create Countline.c

---
+++ Countline.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    int i,n=1;
+    printf("enter string");
+    scanf("%[^\n]s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]=='.')
+        {
+            n++;
+        }
+    }
+    printf("%d",n);
+
+    return 0;
+}
