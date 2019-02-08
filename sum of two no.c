9b7565cfff018066e4a67bc5d5df8e80b392bfb8
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-08 09:11:27 -0800

    Create sum of two no.c

---
+++ sum of two no.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{ 
+    int n1,n2,sum;
+    printf("enter no");
+    scanf("%d%d",&n1,&n2);
+    sum=n1+n2;
+    printf("%d ",sum);
+    return 0;
+}
