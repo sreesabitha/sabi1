0c67df9f85f18f2cc0055e8c1d1daa107d8c9507
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-10 09:52:50 -0800

    Create count digits in integer.c

---
+++ count digits in integer.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,rem,c=0;
+    printf("enter no");
+    scanf("%d",&n);
+    while(n)
+    {
+        rem=n%10;
+        n=n/10;
+        c++;
+    }
+printf("%d",c);
+    return 0;
+}
