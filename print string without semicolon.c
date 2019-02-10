3708146235e43a103838c26040fae0630e9dffbe
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-10 11:43:48 -0800

    Create print string without semicolon.c

---
+++ print string without semicolon.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char s[100];
+    printf("enter string");
+    scanf("%s",&s);
+    if(printf("%s",s))
+    {
+        
+    }
+    
+
+    return 0;
+}
