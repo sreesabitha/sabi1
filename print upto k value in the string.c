f9666d4f17bc6d597f213225c499bc32d4550479
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-10 10:27:16 -0800

    Create print upto k value in the string.c

---
+++ print upto k value in the string.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char s[10];
+    int k,i;
+    printf("enter string");
+    scanf("%d",&k);
+    scanf("%s",&s);
+    for(i=0;i<=k;i++)
+    {
+printf("%c",s[i]);
+}
+    return 0;
+}
