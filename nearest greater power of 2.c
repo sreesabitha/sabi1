3b1cd3cfe4d4e0f375f29a7e9571516d0e414abb
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-10 12:28:01 -0800

    Create nearest greater power of 2.c

---
+++ nearest greater power of 2.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a;
+    printf("enter no");
+    scanf("%d",&n);
+    a=n*2;
+    printf("%d",a);
+
+    return 0;
+}
