0e1e90066eba7fc2d091af7f1b7fab044a74422b
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-10 09:37:20 -0800

    Create print yes if k value exits.c

---
+++ print yes if k value exits.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int s[100];
+    int i,c=0,n,k;
+    printf("enter number");
+    scanf("%d%d",&n,&k);
+    for(i=0;s[i]!='\0';i++)
+    {
+        scanf("%d",&s[i]);
+    }
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]==k)
+        {
+            c++;
+        }
+    }
+    if(c>0)
+    {
+printf("yes");
+}
+else
+{
+    printf("no");
+}
+    return 0;
+}
