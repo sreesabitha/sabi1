503683d766cdfa90d37dadbf592c62d90e74a949
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-11 17:05:05 +0530

    Create Multiple of 13 or not.c

---
+++ Multiple of 13 or not.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter no");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+
+    return 0;
+}
