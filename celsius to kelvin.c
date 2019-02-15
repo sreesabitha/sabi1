662790e153545f3fd806aaaa8becd93e58604f99
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-14 17:39:30 -0800

    Create celsius to kelvin.c

---
+++ celsius to kelvin.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+   scanf("%d",&n);
+   n=n+273;
+   printf("%d",n);
+
+    return 0;
+}
