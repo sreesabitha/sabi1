e712f67c7ae59a6e17ed6c6b8736350155257a69
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-15 16:28:26 +0530

    Create  print power of k.c

---
+++ print power of k.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,k,res;
+    scanf("%d%d",&n,&k);
+    res=pow(n,k);
+    printf("%d",res);
+
+    return 0;
+}
