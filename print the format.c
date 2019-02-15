7b3f6c5c1dbb25638662dc5d768e40ecc464b7c9
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-15 16:20:19 +0530

    Create print the format.c

---
+++ print the format.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,res;
+    scanf("%d%d%d",&a,&b,&c);
+    res=(a*b)/c;
+    printf("%d",res);
+
+    return 0;
+}
