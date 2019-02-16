f2ca2278d89f5b8a5efa9692319441cde2777e8d
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-16 07:40:09 -0800

    Create print ascii value.c

---
+++ print ascii value.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    printf("%d",a);
+
+    return 0;
+}
