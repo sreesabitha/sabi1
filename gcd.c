43f6cbd80e5e2ba48aba300b6b658514aa57359b
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-16 09:34:07 -0800

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,gcd,rem,den,num;
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    {
+        num=a;
+        den=b;
+    }
+    else
+    {
+        num=b;
+        den=a;
+    }
+    rem=num%den;
+    while(rem)
+    {
+        num=den;
+        den=rem;
+        rem=num%den;
+    }
+    gcd=den;
+    printf("%d",gcd);
+ return 0;
+}
