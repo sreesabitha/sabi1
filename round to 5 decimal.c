e685e9c985d84cc27d11399528c2bf26e2b240cb
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-16 07:30:33 -0800

    Create round to 5 decimal.c

---
+++ round to 5 decimal.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    scanf("%f %f",&a,&b);
+    printf("%.5f",b*a);
+
+    return 0;
+}
