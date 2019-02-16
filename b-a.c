929d33537b214e9c67c04fcf0889de72186ba892
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-16 09:57:31 -0800

    Create b-a.c

---
+++ b-a.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d",b-a);
+
+    return 0;
+}
