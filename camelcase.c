c0fcf34286be08afe35d82f7d05c437960a4c31c
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-17 18:09:06 -0800

    Create camelcase.c

---
+++ camelcase.c
@@ -0,0 +1,27 @@
+ #include<stdio.h>
+ int main()
+ {
+ char s[100];
+    int i;
+    scanf("%[^\n]s",s);
+    for(i=0;s[i]!='\0';i++)
+    {
+    if(i==0)
+    {
+        if(s[i]>='a' && s[i]<='z')
+        {
+            s[i]=s[i]-32;
+        }
+    }
+    if(s[i]==' ')
+    {
+    i++;
+    if(s[i]>='a' && s[i]<='z')
+    {
+        s[i]=s[i]-32;
+    }
+    }
+    }
+    printf("%ds",a);
+ return 0;
+ }
