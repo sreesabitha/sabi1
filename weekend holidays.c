4fc9b0d2002eea38338984c4d37ca1d8f2c36e64
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-21 15:48:16 +0530

    Create weekend holidays.c

---
+++ weekend holidays.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char a[10];
+    int i=0;
+    scanf("%s",a);
+    if(a[0]=='s')
+    {
+     printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+
+    return 0;
+}
