15aea2785f846d69a2cce3b704de44640105b873
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-25 08:25:34 +0530

    Create Sum of ap series.c

---
+++ Sum of ap series.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,i,c,sum=0,l;
+
+    scanf("%d %d %d",&a,&c,&b);
+    sum=(b*(2*a+(b-1)*c))/2;
+    l=a+(b-1)*c;
+    for(i=a;i<=l;i=i+c)
+    {
+    
+    
+        
+    
+    
+    }
+        printf("%d",sum);
+    
+    
+
+    return 0;
+}
