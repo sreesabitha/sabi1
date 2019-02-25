2c6ce604a1b3edfa6a6c8b8529a3d7d12d3c5164
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-25 19:51:29 +0530

    Create del vowel and print reverse order.c

---
+++ del vowel and print reverse order.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    char a[100];
+    int c=0,i;
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        c++;
+        if((a[i]=='a')||(a[i]=='e')||(a[i]=='i')||(a[i]=='o')||(a[i]=='u'))
+        {
+           a[i]='\0' ;
+        }
+    }
+    for(i=c;i>=0;i--)
+    {
+      printf("%c",a[i]); 
+    }
+
+    return 0;
+}
