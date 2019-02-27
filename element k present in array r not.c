d2cfa9fde636db7947bee8100e6454936af4ff1a
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-28 01:16:31 +0530

    Create element k present in array r not.c

---
+++ element k present in array r not.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+     int  a[10],k,i,n,c=0;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            c++;
+        }
+    }
+    if(c==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+  return 0;
+}
+
+
+    
