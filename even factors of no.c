8eb12813acf02a021c58dd1c1da3e24c3a823347
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-02-28 02:11:35 +0530

    Create even factors of no.c

---
+++ even factors of no.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+     int n,i,j;
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            if(i%2==0)
+            {
+                printf("%d ",i);
+            }
+        }
+    }
+  return 0;
+}
+
+
+    
