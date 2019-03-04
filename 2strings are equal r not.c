1439b028598376a32575f4899c3333acad2be8f1
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-05 00:35:20 +0530

    Create 2strings are equal r not.c

---
+++ 2strings are equal r not.c
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int main()
+{
+    char a[10],b[10];
+    int i,n,j,flag=0,c=0;
+    scanf("%s %s",a,b);
+    for(i=0;a[i]!='\0';i++)
+    {
+        c++;
+    }
+    j=0;
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==b[j])
+        {
+            flag++;
+            j++;
+        }
+    }
+    if(flag==c)
+    {
+    printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+    
+    
+
+    return 0;
+}
