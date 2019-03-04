6169682eace78d7f3fc6672bf36d7511b64ea205
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-04 23:15:17 +0530

    Create no divisible by any no lessthan n.c

---
+++ no divisible by any no lessthan n.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,n,flag=0;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+
+    return 0;
+}
