272f6d6a76210b406340f9235ff9dcf26f1a35e5
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-10 21:50:22 +0530

    Create area of triangle.c

---
+++ area of triangle.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int b,h,area;
+    scanf("%d %d",&b,&h);
+    area=(0.5*b*h);
+    printf("%d",area);
+
+    return 0;
+}
