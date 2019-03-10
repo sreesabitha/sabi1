89babf35b739f045fb7781295672ed066b4c93a3
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-10 23:59:46 +0530

    Create a^b.c

---
+++ a^b.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,res;
+    scanf("%d %d",&a,&b);
+    res=pow(a,b);
+    printf("%d",res);
+    
+
+    return 0;
+}
