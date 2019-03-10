9200b2aa5c80749a67bdb275a01019991d713795
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-10 12:19:54 +0530

    Create print occurence of kth index.c

---
+++ print occurence of kth index.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int main()
+{
+    char a[10],k[1];
+    int i,j,flag=0,c=0;
+    scanf("%s %c",a,k);
+    for(i=1;a[i]!='\0';i++)
+    {
+        c++;
+    }
+    j=0;
+    for(i=1;i<c;i++)
+    {
+        if(a[i]==k[j])
+        {
+            flag++;
+            break;
+        }
+    }
+    if(flag!=0)
+    {
+    printf("%d",i+1);
+    }
+
+    return 0;
+}
