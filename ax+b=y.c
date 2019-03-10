ee1f23ef0299f184f38c7a30bb254bf8b9dee987
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-10 22:54:21 +0530

    Create ax+b=y.c

---
+++ ax+b=y.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,x,y;
+    scanf("%d %d %d",&a,&b,&x);
+    y=a*x+b;
+    printf("%d",y);
+
+    return 0;
+}
