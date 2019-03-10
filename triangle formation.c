ca1e7d4b9fe7f1583e6b500874dc69d342009210
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-10 23:27:54 +0530

    Create triangle formation.c

---
+++ triangle formation.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if((a+b>c)&&(b+c>a)&&(c+a>b))
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+
+    return 0;
+}
