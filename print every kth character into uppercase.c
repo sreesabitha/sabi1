d6bf61190f256cf59e3e339d4e5e9e8c09798b6e
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-13 21:46:05 +0530

    Create print every kth character into uppercase.c

---
+++ print every kth character into uppercase.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    int i=0,k;
+    scanf("%d",&k);
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if((i+1)%k==0)
+        {
+            a[i]=a[i]-32;
+        }
+        printf("%c",a[i]);
+    }
+
+
+    return 0;
+}
