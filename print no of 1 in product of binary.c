8290795dd088e285b448196e7db0e8d315a3ad90
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-13 22:49:01 +0530

    Create print no of 1 in product of binary.c

---
+++ print no of 1 in product of binary.c
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,rem,rem1,pow=1,pow1=1,product,rem2,c=0,bin=0,bin1=0;
+    scanf("%d %d",&n1,&n2);
+    while(n1)
+    {
+        rem=n1%2;
+        bin=bin+rem*pow;
+        n1=n1/2;
+        pow=pow*10;
+    }
+    while(n2)
+    {
+        rem1=n2%2;
+        bin1=bin1+rem1*pow1;
+        n2=n2/2;
+        pow1=pow1*10;
+    }
+    product=bin*bin1;
+while(product)
+{
+    rem2=product%10;
+    product=product/10;
+    if(rem2==1)
+    {
+        c++;
+    }
+}
+printf("%d",c);
+    return 0;
+}
