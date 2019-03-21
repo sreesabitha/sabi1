1939827f9491072e39e5206aafca9e14db0006b4
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-21 17:52:58 +0530

    Create p75.c

---
+++ p75.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100],n,i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]<a[j])
+            {
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+
+    return 0;
+}
