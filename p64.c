11a4e88bda56bad10b7ef749c0decaf0acc876ae
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-21 07:22:51 +0530

    Create p64.c

---
+++ p64.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a[100],k,t,j;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<k)
+        {
+        printf("%d ",a[i]);
+    }
+    }
+
+    
+    
+
+    return 0;
+}
