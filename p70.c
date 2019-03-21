a4d234479dd7ae1ae25ed5738558b17b92b1c924
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-21 19:03:34 +0530

    Create p70.c

---
+++ p70.c
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+	int n;
+	scanf("%d",&n);
+	int a[100],i,j,diff,min=1,t;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]<a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			diff=a[i]-a[j];
+			if((diff<min)&&(diff!=0))
+			{
+				min=diff;
+			}
+		}
+	}
+	printf("%d",min);
+
+    return 0;
+}
