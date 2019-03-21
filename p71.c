306b50729ef4fc4f05da7cfc595983a43d76ff46
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-21 07:58:41 +0530

    Create p71.c

---
+++ p71.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a[100];
+    printf("enter n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+    if(a[i]>a[i+1])
+    {
+        printf("%d ",a[i]);
+    }
+    else
+    {
+        printf("%d ",a[i+1]);
+    }
+    }
+    
+    
+
+
+    return 0;
+}
