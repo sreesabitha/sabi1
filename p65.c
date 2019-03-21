6121bd6cad7df52a1a51f25b16bd977e3f1c549c
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-21 07:27:16 +0530

    Create p65.c

---
+++ p65.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a[100],j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<n)
+        {
+        printf("%d ",a[i]);
+    }
+    }
+
+    
+    
+
+    return 0;
+}
