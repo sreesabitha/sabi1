a8616d94f8c1c7c38069a6b76cbbd2272ed6e109
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-21 16:22:24 +0530

    Create p73.c

---
+++ p73.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,k,i,a[100];
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            printf("%d",i+1);
+            break;
+        }
+    }
+
+    return 0;
+}
