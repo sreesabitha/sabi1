7c1edbaaf94ef8a97db2665ac1d513cea48e4695
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-22 20:47:55 +0530

    Create p82.c

---
+++ p82.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],i,res=-1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        res=res&a[i];
+    }
+    printf("%d",res);
+
+    return 0;
+}
