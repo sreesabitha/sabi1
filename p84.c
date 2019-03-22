04e24b4b6145e5b8984a507c14e3f409acd8dd02
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-22 19:07:28 +0530

    Create p84.c

---
+++ p84.c
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],i,j,max=0,res;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            res=a[i]|a[j];
+            if(res>max)
+            {
+                max=res;
+            }
+        }
+    }
+        if(n==1)
+        {
+            printf("%d",a[0]);
+        }
+        else
+        {
+    
+        printf("%d",res);
+        }
+    
+    return 0;
+}
