3b6c394554e21bc5fa822fd5aa427c22b53cdb70
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-24 15:26:34 +0530

    Create p69.c

---
+++ p69.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,n,k,a[100];
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-k;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
