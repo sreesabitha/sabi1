c6b9f94762f5e31d45ff12f6b7f2c8e8cd95d150
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-24 17:43:57 +0530

    Create p105.c

---
+++ p105.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],i,t,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d ",i+1);
+    }
+
+    return 0;
+}
