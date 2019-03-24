393eb7a090855eebc34d863df90d894511cb07fa
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-24 17:28:28 +0530

    Create p102.c

---
+++ p102.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],i,sum=0,t;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    t=a[1];
+    a[1]=a[n-1];
+    a[n-1]=t;
+    for(i=0;i<n-1;i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            sum=sum+a[i];
+        }
+        else
+        {
+            sum=sum+a[i+1];
+        }
+    }
+    printf("%d",sum);
+
+    return 0;
+}
