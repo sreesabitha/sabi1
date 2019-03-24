7af62f20c61a72e03b3a17f6d107dfb3155b6be0
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-24 08:07:07 +0530

    Create kth smallest no 108q.c

---
+++ kth smallest no 108q.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],i,j,t,k;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            }
+        }
+    }
+    printf("%d",a[k-1]);
+
+    return 0;
+}
