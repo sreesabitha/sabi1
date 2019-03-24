44011d32c1bdb21dbfa4bdcadf37071bec4fc8c4
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-24 17:34:01 +0530

    Create p104.c

---
+++ p104.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],i,sum=0,t;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+         sum=sum+a[i]+a[i+1];
+    }
+    printf("%d",sum);
+
+    return 0;
+}
