3e00c29504a1d787b57041c45dc58666ae38bd16
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-03-24 17:54:45 +0530

    Create p108.c

---
+++ p108.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,n,sum=0,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+        printf("%d ",sum);
+    }
+
+    return 0;
+}
