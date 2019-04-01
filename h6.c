02acb342b04a1a8b3a441756e9395ba9b1eeed44
sreesabitha <46838737+sreesabitha@users.noreply.github.com>
2019-04-01 18:53:13 +0530

    Create h6.c

---
+++ h6.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100],i,j,n,c=0,b[100],k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+                break;
+            }
+        }
+    if(c!=0)
+    {
+            printf("%d",a[i]);
+            break;
+    }
+    else
+    {
+        printf("unique");
+    }
+    }
+
+
+    return 0;
+}
